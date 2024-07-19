75b97a7e11cdba45a57cd5063a7209b4544adef6
wctsang <147121595+wctsang@users.noreply.github.com>
2024-07-19 10:23:28 +0900

    7-2_確認課題

---
+++ main.cpp
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <thread>
+
+
+using namespace std;
+
+//関数の定義
+
+void Thread1() {
+	cout << "Thread1" << endl;
+}
+
+void Thread2() {
+	cout << "Thread2" << endl;
+}
+
+void Thread3() {
+	cout << "Thread3" << endl;
+}
+
+int main() {
+
+	thread thread1(Thread1);
+	thread1.join();
+
+	thread thread2(Thread2);
+	thread2.join();
+
+	thread thread3(Thread3);
+	thread3.join();
+
+	return 0;
+}
