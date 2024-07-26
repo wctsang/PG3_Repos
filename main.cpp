4e3995b7861745247d9dd183c9954a548e046201
wctsang <147121595+wctsang@users.noreply.github.com>
2024-07-26 10:15:08 +0900

    8-1_確認課題

---
+++ main.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+#include <string>
+#include <chrono>
+
+//コピー時間計算＋出力
+
+void measureCopyTime(const std::string& str) {
+
+    //開始
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    //内容コピー
+
+    std::string copy = str;
+
+    //終了
+
+    auto end = std::chrono::high_resolution_clock::now();
+
+    //時間計算
+
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+
+    //出力
+
+    std::cout << "コピー： " << duration << "μs" << std::endl;
+}
+
+//移動時間計算＋出力
+
+void measureMoveTime(std::string&& str) {
+
+    //開始
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    //内容移動
+
+    std::string moved = std::move(str);
+
+    //終了
+
+    auto end = std::chrono::high_resolution_clock::now();
+
+    //時間計算
+
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+
+    //出力
+
+    std::cout << "移動： " << duration << "μs" << std::endl;
+}
+
+int main() {
+
+    //文字列string with 1000,000 'a'
+
+    std::string a(1000000, 'a');
+
+    std::cout << "1000000文字を移動とコピーで比較しました" << std::endl;
+
+    //コピー
+
+    measureCopyTime(a);
+
+    //移動
+
+    measureMoveTime(std::move(a));
+
+    return 0;
+}
